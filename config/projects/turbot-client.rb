87da9ec8159f367c9946c59103a1da61a0a28c26
Seb Bacon <seb.bacon@gmail.com>
2014-12-12 11:34:29 +0000

    Bring build version in line with turbot version

--- config/projects/turbot-client.rb
+++ config/projects/turbot-client.rb
@@ -3,14 +3,13 @@ maintainer 'OpenCorporates'
 homepage 'https://opencorporates.com'
 
 install_dir     '/opt/turbot-client'
-build_version   '0.1.4'
-build_iteration 2
+build_version   '0.1.6'
+build_iteration 1
 
 # creates required build directories
 dependency 'preparation'
 
 # turbot-client dependencies/components
-# dependency 'somedep'
 
 # version manifest file
 dependency 'version-manifest'
